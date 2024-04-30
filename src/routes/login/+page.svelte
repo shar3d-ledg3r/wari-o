0e7e393eeba66843425f91fd02698af2e32e3576
R3PLiT <144638604+R3PLiT@users.noreply.github.com>
2024-04-30 05:42:27 +0000

    change

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,14 +1,14 @@
 <div class="rendered-form">
     <div class="formbuilder-text form-group field-text-1713845839949">
         <label for="text-1713845839949">Text Field</label>
-        <input type="text" class="form-control" name="text-1713845839949" access="false" id="text-1713845839949">
+        <input type="text" name="text-1713845839949" id="text-1713845839949">
     </div>
     <div class="formbuilder-text form-group field-text-1713845842738">
-        <label for="text-1713845842738" class="formbuilder-text-label">Text Field</label>
-        <input type="text" class="form-control" name="text-1713845842738" access="false" id="text-1713845842738">
+        <label for="text-1713845842738">Text Field</label>
+        <input type="text" name="text-1713845842738" id="text-1713845842738">
     </div>
     <div class="formbuilder-button form-group field-button-1713845848447">
-        <button type="button" class="btn-default btn" name="button-1713845848447" access="false" style="default" id="button-1713845848447">Button</button>
+        <button type="button" name="button-1713845848447" access="false" style="default" id="button-1713845848447">Button</button>
     </div>
 </div>
 
